4f84f08761ec3d43c7a54ca5c2ae5d98a0e53367
Bennett Smrdel <bennett@midwestdiesel.com>
2024-06-05 11:36:19 -0500

    feat: Add scss loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
         exclude: /node_modules/,
         use: 'ts-loader',
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
     ],
   },
   resolve: {
